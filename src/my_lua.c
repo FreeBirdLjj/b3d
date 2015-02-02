57988461997c663a0362266eed8a537e3d54af8c
FreeBirdLjj <ljj11011@mail.ustc.edu.cn>
2015-02-03 00:27:31 +0800

    Nothing but coding style.

--- src/my_lua.c
+++ src/my_lua.c
@@ -2,24 +2,28 @@
 
 #include "my_lua.h"
 
-void warn(const char *s){
+void warn(const char *s)
+{
 	fprintf(stderr, "%s\n", s);
 }
 
 BIND_0_1(warn, string);
 
-int l_reset_menu(lua_State *L){
-	if((luaL_loadbuffer(L, "menu_callbacks={}", /* strlen */ 17 , "cmd"))||lua_pcall(L, 0, 0, 0)){
+int l_reset_menu(lua_State *L)
+{
+	if (luaL_loadbuffer(L, "menu_callbacks={}", /* strlen */ 17 , "cmd") || lua_pcall(L, 0, 0, 0)) {
 		fprintf(stderr, "%s", lua_tostring(L, -1));
 		lua_pop(L, 1);	/* pop error message from the stack */
 	}
+
 	glutCreateMenu(menu_callback);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
 	return 0;
 }
 
-int l_add_menu_item(lua_State *L){
+int l_add_menu_item(lua_State *L)
+{
 	static int index = 0;
 
 	luaL_checktype(L, 2, LUA_TFUNCTION);
@@ -43,18 +47,19 @@ int l_add_menu_item(lua_State *L){
 
 /* Some of the code in this function was taken from Microsoft's
    documentation on their website. */
-void run_process_in_background(const char *command_line){
-
+void run_process_in_background(const char *command_line)
+{
 	char *command_line_copy = strdup(command_line);
-	int len = strlen(command_line);
+	const int len = strlen(command_line);
 
 	/* Append the background symbol '&' if it is not already at the end of the
 	 * string. */
-	if(command_line_copy[len-1]!='&'){
-		command_line_copy = realloc(command_line_copy, len+2);
+	if (command_line_copy[len - 1] != '&') {
+		command_line_copy = realloc(command_line_copy, len + 2);
 		command_line_copy[len] = '&';
-		command_line_copy[len+1] = '\0';
+		command_line_copy[len + 1] = '\0';
 	}
+
 	system(command_line_copy);
 
 	free(command_line_copy);
@@ -62,8 +67,9 @@ void run_process_in_background(const char *command_line){
 
 BIND_0_1(run_process_in_background, string);
 
-int l_get_filename(lua_State *L){
-	char buf[4*80];
+int l_get_filename(lua_State *L)
+{
+	char buf[4 * 80];
 
 	printf("Please enter a filename: ");
 	fgets(buf, sizeof(buf), stdin);
@@ -87,7 +93,8 @@ static const struct luaL_Reg bmlib[] = {
 	{NULL, NULL},
 };
 
-int l_open_bmlib(lua_State *L){
+int l_open_bmlib(lua_State *L)
+{
 	luaL_newlib(L, bmlib);
 	return 1;
 }
@@ -103,18 +110,21 @@ static const luaL_Reg lualibs[] = {
 	{NULL, NULL},
 };
 
-int file_exists(const char filename[]){
+int file_exists(const char filename[])
+{
 	FILE *file = fopen(filename, "r");
 
-	if(!file){
+	if (file == NULL) {
 		return 0;
 	}
+
 	fclose(file);
 
 	return 1;
 }
 
-char *get_current_dir(void){
+char *get_current_dir(void)
+{
 	char *buf = (char *)malloc(80);
 
 	getcwd(buf, 80);
@@ -125,42 +135,46 @@ char *get_current_dir(void){
 /* based on http://www.lua.org/pil/24.1.html */
 /* Global inputs: config_filename, init_filename */
 /* Global outputs: lua_state */
-void brainmaps_start_lua(void){
-
+void brainmaps_start_lua(void)
+{
 	lua_state = luaL_newstate();
-	if(!lua_state){
+
+	if (lua_state == NULL) {
 		fprintf(stderr, "Fatal error: Could not start Lua interpreter.\n");
 		exit(-1);
 	}
 
 	/* Load libraries */
 	luaL_openlibs(lua_state);
+
 	const luaL_Reg *lib;
-	for(lib = lualibs; lib->func; lib++){
-		if(verbosity){
+
+	for (lib = lualibs; lib->func; lib++){
+
+		if (verbosity) {
 			fprintf(stderr, "Loading lua lib: %s\n", lib->name);
 		}
+
 		luaL_requiref(lua_state, lib->name, lib->func, 1);	/* open library */
-		lua_pop(lua_state, 1);				/* discard any results */
+		lua_pop(lua_state, 1);					/* discard any results */
 	}
 
 	l_reset_menu(lua_state);
 
 	/* Search up the directory tree for a while to find the init script. */
-	while(strcmp(get_current_dir(), "/")){
-		if(file_exists(init_filename)){
-			if((luaL_loadfile(lua_state, init_filename))||lua_pcall(lua_state, 0, 0, 0)){
+	while (strcmp(get_current_dir(), "/")) {
+		if (file_exists(init_filename)) {
+			if (luaL_loadfile(lua_state, init_filename) || lua_pcall(lua_state, 0, 0, 0)) {
 				fprintf(stderr, "Fatal error: The init file did not run: %s\n", lua_tostring(lua_state, -1));
 				exit(-1);
-			}
-			else{
+			} else {
 				break;	/* The init file has successfully run.  Our search is over. */
 			}
-		}
-		else{
-			if(verbosity){
+		} else {
+			if (verbosity) {
 				fprintf(stderr,	"%s is not in the current directory.  Changing to parent directory.\n", init_filename);
 			}
+
 			chdir("..");
 		}
 	}
