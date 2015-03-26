3a69358a847b346a93c85070dbfe2767d99de7ac
FreeBirdLjj <ljj11011@mail.ustc.edu.cn>
2015-03-26 10:40:58 +0800

    Add static to `BIND_X_X()`'s result.

--- include/lua_bind.h
+++ include/lua_bind.h
@@ -6,33 +6,33 @@
 /* This macro binds a function with no returns and one argument that maps to a
  * specified Lua type.  The others are analogous, with types t1, t2, etc.
  * -ijt */
-#define BIND_0_1(f, type)	\
-	int l_ ## f(lua_State *L){	\
-		f(luaL_check ## type(L, 1));	\
+#define BIND_0_1(f, t1)	\
+	static int l_ ## f(lua_State *L) {	\
+		f(luaL_check ## t1(L, 1));	\
 		return 0;	\
 	}
 
 #define BIND_0_2(f, t1, t2)	\
-	int l_ ## f(lua_State *L){	\
+	static int l_ ## f(lua_State *L) {	\
 		f(luaL_check ## t1(L, 1), luaL_check ## t2(L, 2));	\
 		return 0;	\
 	}
 
 #define BIND_0_3(f, t1, t2, t3)	\
-	int l_ ## f(lua_State *L){	\
+	static int l_ ## f(lua_State *L) {	\
 		f(luaL_check ## t1(L, 1), luaL_check ## t2(L, 2), luaL_check ## t3(L, 3));	\
 		return 0;	\
 	}
 
 #define BIND_0_4(f, t1, t2, t3, t4)	\
-	int l_ ## f(lua_State* L){	\
+	static int l_ ## f(lua_State* L) {	\
 		f(luaL_check ## t1(L, 1), luaL_check ## t2(L, 2),	\
 		  luaL_check ## t3(L, 3), luaL_check ## t4(L, 4));	\
 		return 0;	\
 	}
 
 #define BIND_0_5(f, t1, t2, t3, t4, t5)	\
-	int l_ ## f(lua_State *L){	\
+	static int l_ ## f(lua_State *L) {	\
 		f(luaL_check ## t1(L, 1), luaL_check ## t2(L, 2),	\
 		  luaL_check ## t3(L, 3), luaL_check ## t4(L, 4),	\
 		  luaL_check ## t5(L, 5));	\
@@ -40,7 +40,7 @@
 	}
 
 #define BIND_0_6(f, t1, t2, t3, t4, t5, t6)	\
-	int l_ ## f(lua_State *L){	\
+	static int l_ ## f(lua_State *L) {	\
 		f(luaL_check ## t1(L, 1), luaL_check ## t2(L, 2),	\
 		  luaL_check ## t3(L, 3), luaL_check ## t4(L, 4),	\
 		  luaL_check ## t5(L, 5), luaL_check ## t6(L, 6));	\
@@ -48,7 +48,7 @@
 	}
 
 #define BIND_0_7(f, t1, t2, t3, t4, t5, t6, t7)	\
-	int l_ ## f(lua_State *L){	\
+	static int l_ ## f(lua_State *L) {	\
 		f(luaL_check ## t1(L, 1), luaL_check ## t2(L, 2),	\
 		  luaL_check ## t3(L, 3), luaL_check ## t4(L, 4),	\
 		  luaL_check ## t5(L, 5), luaL_check ## t6(L, 6),	\
@@ -57,7 +57,7 @@
 	}
 
 #define BIND_0_8(f, t1, t2, t3, t4, t5, t6, t7, t8)	\
-	int l_ ## f(lua_State *L){	\
+	static int l_ ## f(lua_State *L) {	\
 		f(luaL_check ## t1(L, 1), luaL_check ## t2(L, 2),	\
 		  luaL_check ## t3(L, 3), luaL_check ## t4(L, 4),	\
 		  luaL_check ## t5(L, 5), luaL_check ## t6(L, 6),	\
@@ -66,7 +66,7 @@
 	}
 
 #define BIND_0_9(f, t1, t2, t3, t4, t5, t6, t7, t8, t9)	\
-	int l_ ## f(lua_State *L){	\
+	static int l_ ## f(lua_State *L) {	\
 		f(luaL_check ## t1(L, 1), luaL_check ## t2(L, 2),	\
 		  luaL_check ## t3(L, 3), luaL_check ## t4(L, 4),	\
 		  luaL_check ## t5(L, 5), luaL_check ## t6(L, 6),	\
@@ -77,26 +77,26 @@
 
 /* Bind a `function' taking no inputs and returning void. */
 #define BIND_0_0(f)	\
-	int l_ ## f(lua_State *L){	\
+	static int l_ ## f(lua_State *L) {	\
 		f();		\
 		return 0;	\
 	}
 
 /* Bind a function with a single input argument and a non-void return value. */
 #define BIND_1_1(t_out, f, t_in)	\
-	int l_ ## f(lua_State *L){	\
+	static int l_ ## f(lua_State *L) {	\
 		lua_push ## t_out(L, f(luaL_check ## t_in(L, 1)));	\
 		return 1;	\
 	}
 
 #define BIND_1_2(t_out, f, t1, t2)	\
-	int l_ ## f(lua_State *L){	\
+	static int l_ ## f(lua_State *L) {	\
 		lua_push ## t_out(L, f(luaL_check ## t1(L, 1), luaL_check ## t2(L, 2)));	\
 		return 1;	\
 	}
 
 #define BIND_1_3(t_out, f, t1, t2, t3)	\
-	int l_ ## f(lua_State *L){	\
+	static int l_ ## f(lua_State *L) {	\
 		lua_push ## t_out(L,	\
 		                  f(luaL_check ## t1(L, 1), luaL_check ## t2(L, 2),	\
 		                    luaL_check ## t3(L, 3)));	\
@@ -104,7 +104,7 @@
 	}
 
 #define BIND_1_4(t_out, f, t1, t2, t3, t4)	\
-	int l_ ## f(lua_State *L){	\
+	static int l_ ## f(lua_State *L) {	\
 		lua_push ## t_out(L,	\
 		                  f(luaL_check ## t1(L, 1), luaL_check ## t2(L, 2),	\
 		                    luaL_check ## t3(L, 3), luaL_check ## t4(L, 4)));	\
@@ -112,7 +112,7 @@
 	}
 
 #define BIND_1_5(t_out, f, t1, t2, t3, t4, t5)	\
-	int l_ ## f(lua_State *L){	\
+	static int l_ ## f(lua_State *L) {	\
 		lua_push ## t_out(L,	\
 		                  f(luaL_check ## t1(L, 1), luaL_check ## t2(L, 2),	\
 		                    luaL_check ## t3(L, 3), luaL_check ## t4(L, 4),	\
@@ -121,7 +121,7 @@
 	}
 
 #define BIND_1_0(t_out, f)	\
-	int l_ ## f(lua_State *L){	\
+	static int l_ ## f(lua_State *L) {	\
 		lua_push ## t_out(L, f());	\
 		return 1;	\
 	}
